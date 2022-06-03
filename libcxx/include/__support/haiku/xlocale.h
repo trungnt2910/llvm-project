04cf279221b00fe4811444bd1d8c0922bb5beb6e
Trung <57174311+trungnt2910@users.noreply.github.com>
2022-06-03 15:32:07 +0700

    [libcxx] fix(locale): xlocale support for Haiku

---
+++ libcxx/include/__support/haiku/xlocale.h
@@ -0,0 +1,17 @@
+// -*- C++ -*-
+//===-----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP_SUPPORT_HAIKU_XLOCALE_H
+#define _LIBCPP_SUPPORT_HAIKU_XLOCALE_H
+
+#if defined(__HAIKU__)
+#include <__support/xlocale/__strtonum_fallback.h>
+#endif // Haiku
+
+#endif
