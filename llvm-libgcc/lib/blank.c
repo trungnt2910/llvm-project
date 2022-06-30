90e7e2e7bd864dbdb4208ba927ab22d6caabb390
Trung <57174311+trungnt2910@users.noreply.github.com>
2022-06-30 22:05:00 +0700

    [llvm-libgcc]: fix: Properly link glue objects for Haiku

--- llvm-libgcc/lib/blank.c
+++
@@ -1,8 +0,0 @@
-#ifdef __HAIKU__
-#include <os/BeBuild.h>
-#include <SupportDefs.h>
-
-extern uint32 _gSharedObjectHaikuABI = B_HAIKU_VERSION;
-extern uint32 _gSharedObjectHaikuVersion = B_HAIKU_ABI;
-
-#endif
